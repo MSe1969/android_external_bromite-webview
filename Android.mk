97a1b88c1193fe407c6d0f9c6f7901ea5aed2b92
MSe1969 <mse1969@posteo.de>
2018-12-08 13:21:17 +0100

    Initial build - Bromite SystemWebView 71.0.3578.85 (prebuilt)
    See https://www.bromite.org/system_web_view

---
+++ Android.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install the prebuilt 'Bromite' webview apk.
+#   see https://www.bromite.org/
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_OVERRIDES_PACKAGES := webview
+
+LOCAL_MODULE := bromite-webview
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support
+
+LOCAL_MODULE_TARGET_ARCH := arm arm64 
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/SystemWebView.apk
+
+LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
+
+include $(BUILD_PREBUILT)
